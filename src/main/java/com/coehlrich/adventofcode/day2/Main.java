3550816eda1833d323cd0353b71e9df85b1cf3cd
coehlrich <coehlrich@users.noreply.github.com>
2022-12-02 18:46:47 +1300

    Move the common code to a common part and move each scenario to a seperate mapToInt call along with adding a comment for day 2

--- src/main/java/com/coehlrich/adventofcode/day2/Main.java
+++ src/main/java/com/coehlrich/adventofcode/day2/Main.java
@@ -1,5 +1,7 @@
 package com.coehlrich.adventofcode.day2;
 
+import java.util.stream.Stream;
+
 import com.coehlrich.adventofcode.Day;
 import com.coehlrich.adventofcode.Result;
 
@@ -12,48 +14,46 @@ public class Main implements Day {
 
     @Override
     public Result execute(String input) {
-
-        return new Result(calculate(input, false), calculate(input, true));
-    }
-
-    public int calculate(String input, boolean part2) {
-        return input.lines().mapToInt((line) -> {
-            RPS opponent = switch (line.charAt(0)) {
-                case 'A' -> RPS.ROCK;
-                case 'B' -> RPS.PAPER;
-                case 'C' -> RPS.SCISSORS;
-                default -> throw new IllegalStateException();
-            };
-
-            RPS you = switch (line.charAt(2)) {
-                case 'X' -> RPS.ROCK;
-                case 'Y' -> RPS.PAPER;
-                case 'Z' -> RPS.SCISSORS;
-                default -> throw new IllegalStateException();
-            };
-
-            int outcome = 0;
-            if (you == opponent) {
-                outcome = 3;
-            } else if (you.beats() == opponent) {
-                outcome = 6;
-            }
-
-            int part2S = opponent.score() + you.score() - 2;
-            while (part2S < 1) {
-                part2S += 3;
-            }
-            while (part2S > 3) {
-                part2S -= 3;
-            }
-            part2S += switch (you) {
-                case ROCK -> 0;
-                case PAPER -> 3;
-                case SCISSORS -> 6;
-            };
-
-            return !part2 ? (you.score() + outcome) : (part2S);
-        }).sum();
+        Match[] matches = input.lines().map((line) -> new Match(
+                switch (line.charAt(0)) {
+                    case 'A' -> RPS.ROCK;
+                    case 'B' -> RPS.PAPER;
+                    case 'C' -> RPS.SCISSORS;
+                    default -> throw new IllegalStateException();
+                },
+                switch (line.charAt(2)) {
+                    case 'X' -> RPS.ROCK;
+                    case 'Y' -> RPS.PAPER;
+                    case 'Z' -> RPS.SCISSORS;
+                    default -> throw new IllegalStateException();
+                })).toArray(Match[]::new);
+        return new Result(
+                Stream.of(matches)
+                        .mapToInt((match) -> {
+                            int outcome = 0;
+                            if (match.you() == match.opponent()) {
+                                outcome = 3;
+                            } else if (match.you() == match.opponent()) {
+                                outcome = 6;
+                            }
+                            return match.you().score() + outcome;
+                        }).sum(),
+                Stream.of(matches).mapToInt((match) -> {
+                    // In this scenario rock is you lose, paper is you draw, and scissors is you win
+                    int part2S = match.opponent().score() + match.you().score() - 2;
+                    while (part2S < 1) {
+                        part2S += 3;
+                    }
+                    while (part2S > 3) {
+                        part2S -= 3;
+                    }
+                    part2S += switch (match.you()) {
+                        case ROCK -> 0;
+                        case PAPER -> 3;
+                        case SCISSORS -> 6;
+                    };
+                    return part2S;
+                }).sum());
     }
 
 }
---
+++ src/main/java/com/coehlrich/adventofcode/day2/Match.java
@@ -0,0 +1,5 @@
+package com.coehlrich.adventofcode.day2;
+
+public record Match(RPS opponent, RPS you) {
+
+}
